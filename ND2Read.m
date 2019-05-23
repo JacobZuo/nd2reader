84f728fff1dcc4912aed2ec33047c57446c483e4
Jacob Zuo <zwlong@live.com>
2019-05-23 20:02:13 +0800

    Datatype: uint16

--- ND2Read.m
+++ ND2Read.m
@@ -2,7 +2,7 @@ function [ImageStack] = ND2Read(FilePointer, ImagePointer, ImageReadOut, Num)
 
     if ImageReadOut.uiComponents == 1
 
-        ImageStack = zeros([ImageReadOut.uiHeight, ImageReadOut.uiWidth, size(Num, 2)]);
+        ImageStack = zeros([ImageReadOut.uiHeight, ImageReadOut.uiWidth, size(Num, 2)],'uint16');
 
         for i = 1:size(Num, 2)
             [~, ~, ImageReadOut] = calllib('Nd2ReadSdk', 'Lim_FileGetImageData', FilePointer, uint32(Num(i) - 1), ImagePointer);
