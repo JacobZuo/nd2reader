7ce065c519823f3281d4d0a193338abab2ecc685
Jacob Zuo <zwlong@live.com>
2019-06-01 21:42:44 +0800

    Update DisplayBar

--- DisplayBar.m
+++ DisplayBar.m
@@ -9,24 +9,33 @@ function [Percentage, Barlength] = DisplayBar(Index, Length)
             fprintf('-')
         end
 
-        fprintf('%6.2f%%', 0)
-    else
-    end
-
-    fprintf('\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b');
-
-    for i = 1:Barlength
-        fprintf('>')
-    end
-
-    for i = 1:(100 - Barlength)
-        fprintf('-')
-    end
-
-    fprintf('%6.2f%%', Percentage)
-
-    if Barlength == 100
-        fprintf('\n')
+        fprintf('%6.2f%%', Percentage)
     else
 
+        fprintf('\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b');
+        
+        if Barlength == 100
+            for i = 1:100
+                fprintf('#')
+            end
+            
+        elseif Barlength>=1
+            for i = 1:(Barlength - 1)
+                fprintf('#')
+            end
+            fprintf('>')
+        end
+        
+        for i = 1:(100 - Barlength)
+            fprintf('-')
+        end
+        
+        fprintf('%6.2f%%', Percentage)
+        
+        if Barlength == 100
+            fprintf('\n')
+        end
+    
+    end
+        
     end
