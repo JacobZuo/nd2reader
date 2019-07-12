89114112c39af6c97d0b6ba470119e159c4c4830
Jacob Zuo <zwlong@live.com>
2019-07-12 18:28:34 +0800

    Update DisplayBar.

--- DisplayBar.m
+++ DisplayBar.m
@@ -4,27 +4,28 @@ function [Percentage, Barlength] = DisplayBar(Index, Length)
     Barlength = floor(Index / Length * 60);
 
     if Index == 1
-        BarStart='Processing [';
-    elseif Index == Length
-        BarStart = repmat('\b',1,80);
+        tic;
+        BarStart='';
+        TimeSpend=toc;
     else
-        BarStart = repmat('\b',1,68);
+        BarStart = repmat('\b',1,161);
+        TimeSpend=toc;
     end
 
     if Barlength == 60
-        BarText=['Finished!  [', repmat('#',1,60)];
+        BarText=['Finished!  [', repmat('#',1,60),sprintf(']%6.1f', Percentage),'%%'];
     elseif Barlength >= 1
-        BarText=[repmat('#',1,(Barlength - 1)),'>'];
+        BarText=['Processing [', repmat('#',1,(Barlength - 1)),'>',repmat('-',1,(60 - Barlength)),sprintf(']%6.1f', Percentage),'%%'];
     else
-        BarText='';
+        BarText=['Processing [', repmat('-',1,(60 - Barlength)),sprintf(']%6.1f', Percentage),'%%'];
     end
-
-    BarText=[BarStart, BarText,repmat('-',1,(60 - Barlength)),sprintf(']%6.1f', Percentage),'%%'];
-
-    fprintf(BarText)
-
+    
     if Barlength == 60
-        fprintf('\n')
+        TimeSpendText=['Time Used: ', datestr(seconds(TimeSpend),'HH:MM:SS'),' s', '\n'];
+    else
+        TimeSpendText=['Time Used: ', datestr(seconds(TimeSpend),'HH:MM:SS'),' s',repmat(' ',1,10), 'Time Remained: ', datestr(seconds((TimeSpend/Percentage*100)-TimeSpend),'HH:MM:SS'),' s', repmat(' ',1,24)];
     end
+    
+    fprintf([BarStart, BarText, '\n', TimeSpendText])
 
 end
